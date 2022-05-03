f4c837751f1055896213f64b9ac6005f1dea9447
Dang Tran <dang.tran@employmenthero.com>
2022-05-03 13:04:55 +0700

    chore: update data

--- data/index.ts
+++ data/index.ts
@@ -1,36 +1,19 @@
-import lodash from "lodash";
-import { Low, JSONFile } from "lowdb";
-
 interface User {
   id: string;
   name: string;
 }
 
 export interface ConversationDocument {
-  id: string;
-  participantIds: string[];
+  id: number;
   createdAt: string;
+  participant_1: string;
+  participant_2: string;
 }
 
 export interface MessageDoc {
   id: string;
   text: string;
-  sentById: string;
+  sentBy: string;
   conversationId: string;
   createdAt: string;
 }
-
-type Database = {
-  users: Array<User>;
-  messages: Array<MessageDoc>;
-  conversations: Array<ConversationDocument>;
-};
-
-class LowWithLodash<T> extends Low<T> {
-  chain: lodash.ExpChain<this["data"]> = lodash.chain(this).get("data");
-}
-
-const adapter = new JSONFile<Database>("data/messenger.json");
-const db = new LowWithLodash(adapter);
-
-export { db };
